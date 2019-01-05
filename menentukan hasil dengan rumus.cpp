f8ee644c8b6b2780ed0c41a38bdd13eff6bf880b
AminPriadi <45525407+AminPriadi@users.noreply.github.com>
2019-01-05 10:31:02 +0700

    Add files via upload

---
+++ menentukan hasil dengan rumus.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float E,m,s;
+    cout<<"masukan nilai dari m ";
+    cin>>m;
+    cout<<"masukan nilai dari s ";
+    cin>>s;
+
+    E=m*s*s;
+    cout<<"maka nilai m adalah ";
+    cout<<E;
+    return 0;
+}
